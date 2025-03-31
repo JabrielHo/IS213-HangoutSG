dec00c0ba96897119780fb776baaad7103a3d11d
Wynblu <goh.godewynn@gmail.com>
2025-03-31 13:24:10 +0800

    Update community_members_service.sql

--- database/community_members_service.sql
+++ database/community_members_service.sql
@@ -3,7 +3,8 @@ USE community_members_service;
 
 CREATE TABLE IF NOT EXISTS community_members (
     community_id CHAR(36) NOT NULL,
-    user_id VARCHAR(50) NOT NULL,
+    user_id char(36) NOT NULL,
     joined_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        status VARCHAR(20) DEFAULT 'is_in',
     PRIMARY KEY (community_id, user_id)
 );
